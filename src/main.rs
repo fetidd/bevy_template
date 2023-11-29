57a125ed6ead4c98925f9ba829af8476f02a2c96
Ben Jones <63544565+fetidd@users.noreply.github.com>
2023-11-29 21:05:19 +0000

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use bevy::prelude::*;
+use bevy_rapier2d::prelude::*;
+use bevy_editor_pls::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+			     .add_plugins(DefaultPlugins)
+        .add_plugins(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
+        .add_plugins(RapierDebugRenderPlugin::default())
+        .add_plugins(EditorPlugin::default())
+        .run();
 }
